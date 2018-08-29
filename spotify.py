19d9b29b0a7d8f243b14a3250b04be1a6ba9e746
cgrimal <clement.grimal@sigfox.com>
2018-08-29 10:45:22 +0200

    Add playback status icons

--- spotify.py
+++ spotify.py
@@ -7,8 +7,7 @@ import sys
 
 try:
     bus = dbus.SessionBus()
-    spotify = bus.get_object("org.mpris.MediaPlayer2.spotify", "/org/mpris/MediaPlayer2")
-
+    spotify = bus.get_object('org.mpris.MediaPlayer2.spotify', '/org/mpris/MediaPlayer2')
 
     if os.environ.get('BLOCK_BUTTON'):
         control_iface = dbus.Interface(spotify, 'org.mpris.MediaPlayer2.Player')
@@ -20,15 +19,32 @@ try:
             control_iface.Next()
 
     spotify_iface = dbus.Interface(spotify, 'org.freedesktop.DBus.Properties')
-    props = spotify_iface.Get('org.mpris.MediaPlayer2.Player', 'Metadata')
+    status        = spotify_iface.Get('org.mpris.MediaPlayer2.Player', 'PlaybackStatus')
+    metadata      = spotify_iface.Get('org.mpris.MediaPlayer2.Player', 'Metadata')
+
+    artist = metadata['xesam:artist'][0]
+    title  = metadata['xesam:title']
 
     if (sys.version_info > (3, 0)):
-        print(str(props['xesam:artist'][0]) + " - " + str(props['xesam:title']))
+        status = str(status)
     else:
-        print(props['xesam:artist'][0] + " - " + props['xesam:title']).encode('utf-8')
-    exit
+        status = status.encode('utf-8')
+
+    if status == 'Playing':
+        status_icon = u'\uf04b'
+    elif status == 'Paused':
+        status_icon = u'\uf04c'
+    elif status == 'Stopped':
+        status_icon = u'\uf04d'
+    else:
+        status_icon = u''
+
+    if (sys.version_info > (3, 0)):
+        print(status_icon + ' ' + str(artist) + ' - ' + str(title))
+    else:
+        print (status_icon + ' ' + artist + ' - ' + title).encode('utf-8')
+
+    sys.exit(0)
+
 except dbus.exceptions.DBusException:
-    exit
-
-
-
+    sys.exit(1)
